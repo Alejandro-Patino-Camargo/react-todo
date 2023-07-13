081bf2ad8060fc991efa633801b9139322d9c643
alejandro-patino-camargo <alejandropatinoc@gmail.com>
2023-07-13 10:07:18 -0700

    updated terminal mappings to <leader>tt for quick access

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const list = [
   {
-    id: '0',
-    title: 'study vim shortcuts',
-    prority: '1',
-  }
-]
+    id: "0",
+    title: "study vim shortcuts",
+    prority: "1",
+  },
+];
 
 function App() {
-
-  const todoList = list.map(function(item) {
-    return <li key={item.id}>
-      <span>ID:{item.id} </span>
-      <span>{item.title}</span>
-      <span> - priority {item.prority}</span>
-    </li>;
+  const todoList = list.map(function (item) {
+    return (
+      <li key={item.id}>
+        <span>ID:{item.id} </span>
+        <span>{item.title}</span>
+        <span> - priority {item.prority}</span>
+      </li>
+    );
   });
-
 
   return (
     <div>
-
       <h1>Todo List</h1>
       <ul>{todoList}</ul>
-
     </div>
   );
 }
