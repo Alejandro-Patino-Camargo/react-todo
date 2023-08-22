a33b6199ec68f3432915bc8afee4185344aecf6b
Alejandro Patino Camargo <alejandro@Alejandros-MacBook-Pro.local>
2023-08-22 13:01:38 -0700

    lesson-1.4
    Lifting state variables

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -8,7 +8,7 @@ function TodoListItem(props) {
         <span>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span> by {item.author}</span>
+        <span> {item.author}</span>
         <span>{item.num_comments}</span>
         <span>{item.points}</span>
       </li>
