181bc7cfaf2c8c820525b75a6481dafd15842202
Alejandro Patino Camargo <alejandropatinoc@gmail.com>
2023-09-14 20:07:17 -0700

    styling forms in middle of screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import TodoContainer from "./components/TodoContainer";
+import "./App.scss";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <TodoContainer tableName="Your Todos" />
     </div>
   );
